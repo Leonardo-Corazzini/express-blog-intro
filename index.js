1f53c687d9030d37d5ed6ac0306dd8a1dd3341d2
Leonardo-Corazzini <leocora10@gmail.com>
2024-11-18 15:19:59 +0100

    ADD: response json post

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const post = require('./post.js')
 const app = express()
 const port = 3000
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-res.send('Hello World!')
+    res.send('Server del mio blog')
 })
+
+app.get('/bacheca', (req, res) => {
+    res.json(post)
+})
+
 app.listen(port, () => {
 console.log(port)
 })
